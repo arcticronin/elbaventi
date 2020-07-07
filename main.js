0d83f3d70e0256b7e0cd07caa3e8e654800f8cc0
Luca Manzi <ronin.git@gmail.com>
2020-07-07 13:02:33 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,118 @@
+//API
+let apiKey = "b206d46d890589c41e3d52d3ae205375";
+let city = "Portoferraio";
+let uri = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+(document.getElementById("oggi")).addEventListener("click", funz);
+
+function funz() {
+    console.log(uri);
+    var request = new XMLHttpRequest();
+    //request.open( metodo , URL , sincrono? true o false);
+    request.open('GET', uri, true);
+    request.onload = function(){
+        var data = JSON.parse(this.response);
+        var potenzaVento = data.wind.speed;
+        var direzioneVento = data.wind.deg;
+        console.log(data);
+        console.log("potenza= " + potenzaVento);
+        console.log("direzione= " + direzioneVento);
+        console.log("AnalisiVento= " + intToWind(direzioneVento));
+        document.getElementById("poppaVento").innerHTML=(intToWind(direzioneVento)+" "+potenzaVento);
+    }
+    request.send();
+}
+
+function intToWind (degrees){
+  var vento="";
+  if (degrees<= 22.5 || degrees > 337.5)
+    vento = "Tramontana";
+  else if (degrees> 22.5 && degrees <= 67.5 )
+    vento = "Grecale";
+  else if (degrees> 67.5 && degrees <= 112.5 )
+    vento = "Levante"; 
+  else if (degrees> 112.5 && degrees <= 157.5 )
+    vento = "Scirocco"; 
+  else if (degrees> 157.5 && degrees <= 202.5 )
+    vento = "Mezzogiorno"; 
+  else if (degrees> 202.5 && degrees <= 247.5 )
+    vento = "Libeccio"; 
+  else if (degrees> 247.5 && degrees <= 292.5 )
+    vento = "Levante"; 
+  else if (degrees> 292.5 && degrees <= 337.5 )
+    vento = "Levante"; 
+  return vento;
+
+}
+
+
+
+
+
+//effetto ripple
+const buttons = document.querySelectorAll('a');
+buttons.forEach(btn=>{
+  btn.addEventListener('click', function(e){
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripples = document.createElement('span');
+    ripples.style.left = x +'px';
+    ripples.style.top = y +'px';
+    this.appendChild(ripples);
+
+    setTimeout(()=>{
+      ripples.remove()
+    },1000)
+})
+})
+
+/*script per ajax 
+
+const form = {
+  devocercare: document.getElementById('btn-mima'),
+  parola: document.getElementById('parola-da-ind'),
+  removable: document.getElementById('removable')
+ };
+
+ console.log(form);
+
+form.devocercare.addEventListener('click', () =>{
+  console.log('button pressed~!');
+  const request = new XMLHttpRequest();
+
+  request.onload = ()=> {
+    console.log(request.responseText);
+    let quellochemitorna = null;
+    try {
+      quellochemitorna = JSON.parse(request.responseText);
+    } catch (e) {
+      console.error('errore, sono nel blocco catch');
+    }
+    if (quellochemitorna){
+      console.log("sono dentro a if...eseguiresponse");
+      eseguisuresponse(quellochemitorna);
+    }
+  };
+
+  console.log('sto mandando la richiesta');
+  request.open('post', 'search.php');
+  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  request.send();
+
+});
+
+function eseguisuresponse(quellochemitorna){
+    console.log("sono dentro a eseguiresponse");
+    console.log(quellochemitorna);
+    //mi svuota la lista, potrei anche lasciarlo
+    while(form.parola.firstChild){
+      form.parola.removeChild(form.parola.firstChild);
+    }
+     const li = document.createElement('li');
+     li.textContent = quellochemitorna.parola;
+     form.parola.appendChild(li);
+     form.parola.style.display = "block";
+     form.removable.style.display = "none";
+
+  }*/
---
+++ search.php
@@ -0,0 +1,38 @@
+<?php
+
+  //$devocercare = isset($_POST['action'])
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $messages = array();
+  $parola = '';
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        $messages[] = 'Connessione malissimo';
+    }
+    else 
+      $messages[] = 'Connessione ochei-fully';       
+    
+    //echo '</p>';
+    $sql = "SELECT * FROM parole.parole ORDER BY RAND() LIMIT 1";
+    $result = $conn->query($sql);
+    
+    if ($result) {
+      $row = $result->fetch_assoc();
+      $parola = $row["p"];
+    } else {
+    $messages[] = 'No result';
+    }
+    $conn->close();
+    echo json_encode(
+      array(
+        'parola' => $parola,
+        'messages' => $messages
+      )
+      );
+?>
